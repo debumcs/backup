b156db5378064af650691d87d15ed5234e78e268
debumcs <debumcs@gmail.com>
2017-06-17 16:33:58 +0530

    Delete email.bat

--- email.bat
+++
@@ -1,5 +0,0 @@
-::email.bat:::::::::::::::::::::::::::::::::::::::::::::::::::::
-@echo off
-
-SendEmail -f "EMT.Admin@quatrro.com" -t "p-fs0121@internal.quatrro.com" -u "Test Subject" -m "Test Message" -q -s 10.100.4.153:25 -xu "EMT.Admin@quatrro.com" -xp Passw0rd
-PAUSE
